669ffa7c3797ba466b73b4f609a41380cd5f27e8
PHY800 <238r1a05v1@cmrec.ac.in>
2024-11-12 10:27:11 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,9 @@
+import java.io*
+import java.util.*;
+public class solution{
+  Public static void main(Strings[] args)
+  { 
+    Scanner sc = new Scanner(System.in);
+    System.out.println("hello");}
+  
+    
